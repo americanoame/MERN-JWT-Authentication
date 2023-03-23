a5a1f7afdc441bcfe73ff05fe480ba92ff129bb9
Americano.Dev <andrenations@gmail.com>
2023-03-23 03:29:36 -0400

    added packages & connected to the database

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const connectDb = require('./conf/db')
+require('dotenv').config()
+const app = express();
+const PORT = 8000
+
+connectDb()
+app.listen(PORT, () => {
+    console.log(`API is running on pot ${PORT}`);
+ })
+ 
