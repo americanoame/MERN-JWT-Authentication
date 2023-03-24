5baf6987063a3c77ea65f6aa2cdcca7c843625a9
Americano.Dev <andrenations@gmail.com>
2023-03-24 13:14:49 -0400

    added login page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
+
+
 function App() {
   return (
     <div className="App">
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,36 @@
 import React from 'react'
 
 function Login() {
-  return (
-    <h1>Login</h1>
-  )
+    return (
+
+        <div className='row login-style py-5'>
+            <div className="d-flex justify-content-center">
+                <div className="col-12 col-sm-8 col-lg-6 mx-auto">
+                    <h1 className="text-center">Login <span className='symbols'>&dArr;</span></h1>
+                    <form className='form-style mt-4'>
+                        <div className='form-group mt-2'>
+                            <h5>email</h5>
+                            <input type="email" className='form-control' placeholder='Enter 
+                             Your Email' required />
+                        </div>
+
+                         <div className='form-group mt-2'>
+                             <h5>Password</h5>
+                            <input type="password" className='form-control' placeholder='Enter 
+                             Your Password' required />
+                        </div>
+
+                        
+
+                 </form>
+                </div>
+            </div>
+        </div>
+
+    )
 }
 
 export default Login
+
+// onSubmit={loginSubmit}
+
