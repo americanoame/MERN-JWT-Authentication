7d917c3003636a0c3ae7d07cc37ef9efd9b6fa4a
Americano.Dev <andrenations@gmail.com>
2023-03-28 19:08:03 -0400

    registered user with frontend

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -43,35 +43,7 @@ export default Login
 
 
 
-// <div className='row login-style py-5'>
-        //     <div className="d-flex justify-content-center">
-        //         <div className="col-12 col-sm-8 col-lg-6 mx-auto">
-        //             <h1 className="text-center">Login <span className='symbols'>&#128511;</span></h1>
-        //             <form className='form-style mt-4'>
-        //                 <div className='form-group mt-3'>
-        //                     <h5>Email</h5>
-        //                     <input type="email" className='form-control' placeholder='Enter 
-        //                      Your Email' />
-        //                 </div>
 
-        //                 <div className='form-group mt-3'>
-        //                     <h5>Password</h5>
-        //                     <input type="password" className='form-control' placeholder='Enter 
-        //                      Your Password' />
-        //                 </div>
+        
 
-        //                 <div>
-        //                     <button className='mt-4 btn-btn'>Submit</button>
-        //                 </div>
-
-        //                 <span className='sign-up-link'>
-        //                     If you don't an account! <Link to='/register'>SignUp</Link>
-        //                 </span>
-        //             </form>
-        //         </div>
-        //     </div>
-        // </div>
-
-
-// onSubmit={loginSubmit} line 12
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,154 +1,109 @@
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
+import axios from 'axios'
 
 function Register() {
 
-  const { register, formState: {errors}, handleSubmit,} = useForm();
+  const { register, formState: { errors }, handleSubmit, } = useForm();
   const onSubmit = (data) => console.log(data);
 
-   const registerSubmit = async(data) => {
-    if(data.password === data.cpassword) {
-         const userData = {
-           user : data.user,
-           email: data.email,
-           password : data.password
-         }
-         console.log(userData);
-    }else{
-       alert('Password does not match')
-    }
-   }
+  const registerSubmit = async (data) => {
+    console.log(data);
+    if (data.password === data.cpassword) {
+      const userData = {
+        user: data.name,
+        email: data.email,
+        password: data.password
+      }
+      console.log(userData);
+      await axios.post('auth/register', userData)
+        .then(log => {
+          if (log.data.success) {
+            alert('User registered successfully')
 
+          } else {
+
+            alert('Something went wrong')
+          }
+        })
+        .catch(error => {
+          alert(error)
+        })
+    } else {
+      alert('Password does not match')
+    }
+  }
 
 
   return (
 
-<div className='login-style py-5'>
+    <div className='login-style py-5'>
 
-<div class="form-container">
- <form onSubmit={handleSubmit(onSubmit)} className='form-style mt-4'>
-    <h1 className="text-center">SignUp<span className='symbols'>&#128275;</span></h1>
-        <div className="mb-3">
-        <h5>Name</h5>
-               <input type="name" className='form-control b-ground' placeholder='Enter Name'
-                 {...register('name',{required: true, minLength: 6})} />
-              <error>
-               {errors.name?.type && <p className='text-danger mt-1'>Name should be at least 6 characters</p>}
-               </error>
-        </div>
-        
-        <div className="mb-3">
-        <h5>Email address</h5>
-              <input type="email" className='form-control' placeholder='Enter email' 
-               {...register('email', {required: true, pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/ })} />
-                <error>    
-                 {errors.email?.type === 'required' && <p className='text-danger mt-1'>Email is required</p>}
-                 {errors.email?.type === 'pattern' && <p className='text-danger mt-1'>Entered Email is in wrong format</p>}
-              </error>
-        </div>
+      <div className="form-container">
+        <form onSubmit={handleSubmit(registerSubmit)} className='form-style mt-4'>
+          <h1 className="text-center">SignUp<span className='symbols'>&#128275;</span></h1>
+          <div className="mb-3">
+            <h5>Name</h5>
+            <input type="name" className='form-control b-ground' placeholder='Enter Name'
+              {...register('name', { required: true, minLength: 6 })} />
+            <div>
+              {errors.name?.type && <p className='text-danger mt-1'>Name should be at least 6 characters</p>}
+            </div>
+          </div>
 
-        <div className="mb-3">
-         <h5>Password</h5>
-               <input type="password" className='form-control' placeholder=' 
-                Password' {...register('password', {required: true, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#%&])(?=.{8,})/
-                })} />
+          <div className="mb-3">
+            <h5>Email address</h5>
+            <input type="email" className='form-control' placeholder='Enter email'
+              {...register('email', { required: true, pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/ })} />
+            <div>
+              {errors.email?.type === 'required' && <p className='text-danger mt-1'>Email is required</p>}
+              {errors.email?.type === 'pattern' && <p className='text-danger mt-1'>Entered Email is in wrong format</p>}
+            </div>
+          </div>
 
-               <error>    
-                  {errors.password?.type === 'required' && <p className='text-danger mt-1'>Password must be at least 8 character </p>}
-                  {errors.password?.type === 'pattern' && <p className='text-danger mt-1'>Should contain at least one uppercase, lowercase, number and special character</p>}
-               </error>
-             </div>
+          <div className="mb-3">
+            <h5>Password</h5>
+            <input type="password" className='form-control' placeholder=' 
+                Password' {...register('password', {
+              required: true, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#%&])(?=.{8,})/
+            })} />
 
-             <div className="mb-3">
-               <h5>Confirm Password</h5>
-               <input type="password" className='form-control' placeholder=' 
-              Conform Password' {...register('cpassword', {required: true, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#%&])(?=.{8,})/
-                })} />
+            <div className='text-danger mt-1'>
+              {errors.password?.type === 'required' && <p>Password must be at least 8 character </p>}
+              {errors.password?.type === 'pattern' && <p>Should contain at least one uppercase, lowercase, number and special character</p>}
+            </div>
+          </div>
 
-               <error>    
-                  {errors.password?.type === 'required' && <p className='text-danger mt-1'>Password must match with the previous one </p>}
-                 {errors.password?.type === 'pattern' && <p className='text-danger mt-1'>Should contain at least one uppercase, lowercase, number and special character</p>}
-               </error>
-             </div>
-        
-             <div className="d-flex justify-content-center ">
-               <button className='mt-2 btn-btn'>Submit</button>
-             </div>
+          <div className="mb-3">
+            <h5>Confirm Password</h5>
+            <input type="password" className='form-control' placeholder=' 
+              Conform Password' {...register('cpassword', {
+              required: true, validate: (value, formValues) => value === formValues.password
+            })} />
 
-             <span className='sign-up-link'>
-               Already Signup? <Link to='/login'>Login</Link>
-             </span>
-    </form>
-</div>
+            <div className='text-danger mt-1'>
+              {errors.cpassword?.type === 'validate' && <p >Password must match with the previous one </p>}
+            </div>
+          </div>
 
-</div>
+          <div className="d-flex justify-content-center ">
+            <button className='mt-2 btn-btn'>Submit</button>
+          </div>
 
-)
+          <span className='sign-up-link'>
+            Already Signup? <Link to='/login'>Login</Link>
+          </span>
+        </form>
+      </div>
+
+    </div>
+
+  )
 }
 
 export default Register;
 
 
-    // <div className='row login-style py-5'>
-    //   <div className="d-flex justify-content-center">
-    //     <div className="col-12 col-sm-8 col-lg-6 mx-auto">
-    //       <h1 className="text-center">SignUp <span className='symbols'>&#128511;</span></h1>
-    //       <form onSubmit={handleSubmit(onSubmit)} className='form-style mt-4'>
-    //         <div className='form-group mt-2'>
-    //           <h5>Name</h5>
-    //           <input type="name" className='form-control' placeholder='Enter Name'
-    //             {...register('name',{required: true, minLength: 6})} />
-    //           <error>
-    //             {errors.name?.type && <p className='text-danger mt-1'>Name should be at least 6 characters</p>}
-    //           </error>
-    //         </div>
-
-    //         <div className='form-group mt-4'>
-    //           <h5>Email address</h5>
-    //           <input type="email" className='form-control' placeholder='Enter email' 
-    //           {...register('email', {required: true, pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/ })} />
-    //             <error>    
-    //              {errors.email?.type === 'required' && <p className='text-danger mt-1'>Email is required</p>}
-    //              {errors.email?.type === 'pattern' && <p className='text-danger mt-1'>Entered Email is in wrong format</p>}
-    //           </error>
-    //         </div>
-
-    //         <div className='form-group mt-4'>
-    //           <h5>Password</h5>
-    //           <input type="password" className='form-control' placeholder=' 
-    //            Password' {...register('password', {required: true, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#%&])(?=.{8,})/
-    //            })} />
-
-    //           <error>    
-    //              {errors.password?.type === 'required' && <p className='text-danger mt-1'>Password must be at least 8 character </p>}
-    //              {errors.password?.type === 'pattern' && <p className='text-danger mt-1'>Should contain at least one uppercase, lowercase, number and special character</p>}
-    //           </error>
-    //         </div>
-
-    //         <div className='form-group mt-4'>
-    //           <h5>Confirm Password</h5>
-    //           <input type="password" className='form-control' placeholder=' 
-    //          Conform Password' {...register('cpassword', {required: true, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#%&])(?=.{8,})/
-    //            })} />
-
-    //           <error>    
-    //              {errors.password?.type === 'required' && <p className='text-danger mt-1'>Password must be at least 8 character </p>}
-    //              {errors.password?.type === 'pattern' && <p className='text-danger mt-1'>Should contain at least one uppercase, lowercase, number and special character</p>}
-    //           </error>
-    //         </div>
-
-
-    //         <div>
-    //           <button className='mt-4 btn-btn'>Submit</button>
-    //         </div>
-
-    //         <span className='sign-up-link'>
-    //           Already Signup? <Link to='/login'>Login</Link>
-    //         </span>
-    //       </form>
-    //     </div>
-    //   </div>
-    // </div>
 
 
 
@@ -158,47 +113,15 @@ export default Register;
 
 
 
-// handleSubmit on line 10
-
-// /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|
-//                     (".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|
-//                     (([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
-// {...register('user', { required: true, minLength: 6 })} />
-//               {errors.user && <p className='text-danger mt-1'>Name should be of at least 6 characters</p>}
 
 
-// required {...register('email',
-//                 {
-//                   required: true,
-//                   pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-
-//                 })} />
-
-//               {errors.email && <p className='text-danger mt-1'>Please enter your correct email</p>}
 
 
-// {...register('password',
-//                 {
-//                   required: true,
-//                   pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/
-                  
-//                 })} />
-//                 {errors.password && <div>
-//                   <p className='text-danger mt-1'>Password should be of length 6-20</p>
-//                   <p>Should contain at least one uppercase, lowercase, number and special character</p>
-//                 </div>
-//               }
 
 
-// {...register('cpassword',
-//                 {
-//                   required: true,
-//                   pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/
-                  
-//                 })} />
-//                 {errors.password && <div>
-//                   <p className='text-danger mt-1'>Password should be of length 6-20</p>
-//                   <p>Should contain at least one uppercase, lowercase, number and special character</p>
-//                 </div>
-//               }
+
+
+
+
+
+
