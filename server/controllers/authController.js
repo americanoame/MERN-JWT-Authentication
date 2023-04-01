be4df61f2b31354a5f57b7d1a91a4b1ffcd3da6a
Americano.Dev <andrenations@gmail.com>
2023-04-01 01:01:50 -0400

    hashing passowrd

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/UserModel');
+const bcrypt = require('bcryptjs');
 
 const registerUser = async (req, res) => {
     const { user, email, password } = req.body
@@ -6,37 +7,90 @@ const registerUser = async (req, res) => {
     const userExist = await User.findOne({ email })
 
     if (userExist) {
-        return res.status(200).send({ success: false, msg : 'User already register with this email' })
+        return res.status(200).send({ success: false, msg: 'User already register with this email' })
     } else {
         try {
-            const newEntry = new User(req.body)
-            newEntry.save()
-            console.log(newEntry)
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword = await bcrypt.hash(password, salt)
+            const newEntry = await User.create({
+                user: user,
+                email: email,
+                password: hashedPassword
+            })
+            // const newEntry = new User(req.body)
+            // newEntry.save()
+            // console.log(newEntry)
 
-            return res.status(200).send({success: true, msg : 'Registration successful'})
+            return res.status(200).send({ success: true, msg: 'Registration successful' })
         } catch (error) {
-            return res.status(400).send({success: false, msg : 'error'}) 
+            console.error(error)
+            return res.status(400).send({ success: false, msg: 'error', data: { error } })
         }
     }
 }
 
 const loginUser = async (req, res) => {
- const {email, password} = req.body
-   try {
-    const user = await User.find({email, password})
-    if(user) {
-      return  res.status(200).send({success: true, msg: 'Login successful'})
-    } else{
-        return res.send({success: false, msg: 'Invalid credentials'})
-    }
-   } catch (error) {
-      return res.send(error) 
-   }
+    const { email, password } = req.body
+    try {
+        //get user
+        const user = await User.findOne({ email })
+        if (user) {
+            if (user && (await bcrypt.compare(password, user.password))) {
+                return res.status(200).send({ success: true, msg: 'Login successful' })
 
+            } else {
+                return res.status(200).send({ success: false, msg: 'Invalid credentials' })
+            }
+
+        } else {
+            return res.send(200).send({ success: false, msg: 'Invalid credentials' })
+        }
+    } catch (error) {
+        return res.send(error)
+    }
 
 }
+
+
 
 module.exports = {
     registerUser,
     loginUser
 }
+
+
+
+
+
+// const loginUser = async (req, res) => {
+//     const { email, password } = req.body
+//     try {
+//         //get user
+//         const user = await User.findOne({ email })
+//         if (!user) {
+//             return res.json('Invalid credentials', 401)
+//         }
+//         //get hashed password
+//         const storedPassHash = user.password
+
+//         //take salt from hash
+//         //has new password
+//         const inputPasswordHash = await bcrypt.hash(password, storedPassHash.slice(0, 29))
+
+//         //compare with stored pass word
+//         if (inputPasswordHash !== storedPassHash) {
+//             return res.json('Invalid credentials', 401)
+
+//         }
+
+//         if (user) {
+//             return res.status(200).send({ success: true, msg: 'Login successful' })
+//         } else {
+//             return res.send({ success: false, msg: 'Invalid credentials' })
+//         }
+//     } catch (error) {
+//         return res.send(error)
+//     }
+
+
+// }
